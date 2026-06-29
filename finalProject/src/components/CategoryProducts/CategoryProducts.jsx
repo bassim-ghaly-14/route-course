2f85a03aafaf2d5ad2244e459eb2dda91c66bc71
Bassim Ghaly <bassim.ghaly@gmail.com>
2026-06-29 17:27:11 +0300

    commit

--- finalProject/src/components/CategoryProducts/CategoryProducts.jsx
+++ finalProject/src/components/CategoryProducts/CategoryProducts.jsx
@@ -45,7 +45,7 @@ export default function CategoryProducts() {
 
       {products.length === 0 ? (
         <div className="text-center text-gray-500 py-10">
-          No products found in this category
+          oducts found in this category
         </div>
       ) : (
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
